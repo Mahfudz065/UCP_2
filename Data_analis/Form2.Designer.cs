a66d713df483c7ce515b720f60da2f1a03757cda
TC1034 <mahfudzsiddiq0@gmail.com>
2023-06-15 14:49:44 +0700

    Menambahkan bcakground image

--- Data_analis/Form2.Designer.cs
+++ Data_analis/Form2.Designer.cs
@@ -69,6 +69,8 @@ namespace Data_analis
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
